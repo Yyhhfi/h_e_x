afc23d3b9c8ad15d7a567560c7479ad719cb3819
Yyhhfi <125329698+Yyhhfi@users.noreply.github.com>
2023-05-29 11:43:23 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,39 @@
+termux-setup-storage
+clear
+apt update 
+clear
+apt install sox
+clear
+apt install figlet
+clear
+apt install python python2
+clear
+apt install python3
+clear
+apt install git
+clear
+apt install wget
+clear
+apt install php
+clear
+apt install proot-distro
+clear
+proot-distro install ubuntu
+clear
+apt install curl
+clear
+apt install openssh
+clear
+pip install gtts
+clear
+pip install requests
+clear
+pip install pytube
+clear
+pip install compileall
+clear
+pip install scapy
+clear
+pip install django
+clear
+
